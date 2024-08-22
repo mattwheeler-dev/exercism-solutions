fe8c517bc875dca2dc73760196d682d2612536fe
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-08-22 13:34:02 -0700

    Add Exercise: Collatz Conjecture (Python)

---
+++ collatz-conjecture.py
@@ -0,0 +1,15 @@
+def steps(number):
+    steps = 0
+    if number == 1:
+        return steps
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+      
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = (number * 3) + 1
+        steps += 1
+        
+    return steps
